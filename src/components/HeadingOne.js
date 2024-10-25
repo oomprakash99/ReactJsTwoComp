9640e85f666b5d088c59c16d1835e18b82166adf
oomprakash <oomprakash99@gmail.com>
2024-10-25 18:22:53 +0530

    upload

---
+++ src/components/HeadingOne.js
@@ -0,0 +1,8 @@
+// src/HeadingOne.js
+import React from 'react';
+
+const HeadingOne = () => {
+  return <h1>Heading 1</h1>;
+};
+
+export default HeadingOne;
---
+++ src/components/HeadingTwo.js
@@ -0,0 +1,8 @@
+// src/HeadingTwo.js
+import React from 'react';
+
+const HeadingTwo = () => {
+  return <h2>Heading 2</h2>;
+};
+
+export default HeadingTwo;
